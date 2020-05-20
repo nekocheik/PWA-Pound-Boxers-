9b78d460f341ed82c663b8f70afeb11bd36d4fd2
Maevabzn <44022913+Maevabzn@users.noreply.github.com>
2020-05-20 13:17:07 +0200

    favicon

--- vue.config.js
+++ vue.config.js
@@ -6,14 +6,14 @@ module.exports = {
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
     manifestOptions: {
-      display: 'fullscreen',
+      display: 'fullscreen'
     },
     iconsPath: {
       favicon16: '/public/img/icons/favicon.ico',
       favicon32: '/public/img/icons/favicon.ico',
       appleTouchIcon: '/public/img/icons/apple-icon-152x152.png',
       maskIcon: '/public/img/icons/favicon.ico',
-      msTileImage: '/public/img/icons/ms-icon-144x144.png',
+      msTileImage: '/public/img/icons/ms-icon-144x144.png'
     },
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
